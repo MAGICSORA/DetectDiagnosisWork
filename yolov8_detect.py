8b7a3b723d4c2a2ff21241c26f18bd79776be418
TaeYoon17 <kim0517@hotmail.com>
2023-04-17 18:29:44 +0900

    Complete_yolov8_detect_to_web

--- yolov8_detect.py
+++ yolov8_detect.py
@@ -2,32 +2,33 @@ import cv2
 from ultralytics import YOLO
 import torch
 import os
+def example():
+    now_path = os.path.abspath(".")
+    model = YOLO(now_path+"/best.pt")
+    img_path = os.path.abspath(".")+'/V006_77_0_00_01_01_13_0_c03_20201209_0000_S01_1.jpg'
+    inputs = [img_path]
+    result = model(inputs)[0]
+    result = result.cpu()
+    #predict = model.predict(source=img_path, show=False, show_labels=True, show_conf=True, conf=0.3)
+    boxes = result.boxes
+    masks = result.masks
+    probs = result.probs
+    print("-------------boxes----------------")
+    #box 좌표
+    print(boxes.xyxyn)
+    print("----------boxes conf ------------")
+    #box 정확도
+    print(boxes.conf)
+    #box 분류한 값
+    print("-----------boxes class --------------")
+    print(boxes.cls)
 
-now_path = os.path.abspath(".")
-model = YOLO(now_path+"/best.pt")
-img_path = os.path.abspath(".")+'/V006_77_0_00_01_01_13_0_c03_20201209_0000_S01_1.jpg'
-inputs = [img_path]
-result = model(inputs)[0]
-result = result.cpu()
-#predict = model.predict(source=img_path, show=False, show_labels=True, show_conf=True, conf=0.3)
-boxes = result.boxes
-masks = result.masks
-probs = result.probs
-print("-------------boxes----------------")
-#box 좌표
-print(boxes.xyxyn)
-print("----------boxes conf ------------")
-#box 정확도
-print(boxes.conf)
-#box 분류한 값
-print("-----------boxes class --------------")
-print(boxes.cls)
 
 class Detector:
     def load_model(self,model_path):
         if model_path:
             try:
-                model = YOLO(os.path.abspath(".")+model_path)
+                model = YOLO(os.path.abspath(".")+"/"+model_path)
                 return model
             except:
                 print("Model Name is Wrong!!")
@@ -56,7 +57,7 @@ class Detector:
                 "bbox":[float(v) for v in box.xyxyn[0].tolist()]
             },sorted(boxes,key=lambda x: float(x.conf))[:3])
         )
-    def __call__(self,crop_type,img_path):
+    async def __call__(self,crop_type,img_path):
         result = self.model([img_path])[0]
         #여기 수정하기 -- 필터링 로직
         boxes = list(result.boxes)
@@ -64,4 +65,7 @@ class Detector:
         #boxes = list(filter(lambda box: True if box.cls in crop_type_match[crop_type] else False, boxes))
         boxes = self.getMyDiagnosis(crop_type,boxes)
         if not boxes: return self.empty_dict
-        return self.getTop3Diagnosis(boxes)
+        return {
+            "responseCode" : 0,
+            "diagnoseReults": self.getTop3Diagnosis(boxes)
+        }
--- yolov8_detect_to_web.py
+++ yolov8_detect_to_web.py
@@ -1,36 +1,44 @@
 import os
 import time
 from threading import Thread
-#from yolov8_detect import Detector
-#detector = Detector("./best.pt")
-img_path = os.path.abspath(".")+'/V006_77_0_00_01_01_13_0_c03_20201209_0000_S01_1.jpg'
-#dicts = detector(0,img_path)
+from queue import Queue
+from yolov8_detect import Detector
+import asyncio
+import json
 
 class AI_Server:
     def __init__(self):
-        #self.detector = Detector("./best.pt")
-        self.work_queue = []
+        self.detector = Detector("./best.pt")
+        self.work_queue = Queue()
         self.isRunning = False
         self.backThread = Thread(target=self.runner)
 
     def runner(self):
-        while self.work_queue:
-            data = self.work_queue.pop(0)
-            self.sendToClient(data)
+        while not self.work_queue.empty():
+            json_file,img_file = self.work_queue.get()
+            user_data = json.loads(json_file)
+            result = asyncio.run(self.detector(user_data["imageCode"],img_file))
+            result["cropImageID"] = user_data["cropImageID"]
+            self.sendToClient(result)
         self.isRunning = False
 
-    def receiveToClient(self,json_file):
+    def receiveToClient(self,json_file,img_file):
         print("receive" + str(json_file))
-        self.work_queue.append(json_file)
+        self.work_queue.put((json_file,img_file))
         if not self.backThread.is_alive():
             self.isRunning = True
             self.backThread.start()
 
-    def sendToClient(self,json_file):
-        time.sleep(1)
-        print("hello" + str(json_file))
+    def sendToClient(self,result):
+        print(json.dumps(result))
         return
+    
+
 worker = AI_Server()
-for i in range(0,11):
-    worker.receiveToClient(i)
-    #if worker.
+for img in [os.path.abspath(".")+v for v in ['/V006_77_0_00_01_01_13_0_c03_20201209_0000_S01_1.jpg'
+,'/V006_77_0_00_01_01_13_0_c03_20201209_0188_S01_1.jpg']]:
+    json_file = json.dumps({
+        "cropImageID": "hello12345",
+        "imageCode": 0
+    })
+    worker.receiveToClient(json_file,img)
