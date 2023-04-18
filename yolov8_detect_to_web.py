ae15fe532897ecf77927f5eb58a2e9d02e11e183
TaeYoon17 <kim0517@hotmail.com>
2023-04-18 17:19:00 +0900

    Check_Multi_Processing

--- yolov8_detect_to_web.py
+++ yolov8_detect_to_web.py
@@ -13,6 +13,7 @@ class AI_Server:
         self.isRunning = False
         self.backThread = Thread(target=self.runner)
 
+    # 백 쓰레드가 실행할 작업들
     def runner(self):
         while not self.work_queue.empty():
             json_file,img_file = self.work_queue.get()
@@ -22,21 +23,34 @@ class AI_Server:
             self.sendToClient(result)
         self.isRunning = False
 
+    #메인 서버 정보를 플라스크에서 받는 함수
     def receiveToClient(self,json_file,img_file):
-        print("receive" + str(json_file))
         self.work_queue.put((json_file,img_file))
         if not self.backThread.is_alive():
             self.isRunning = True
             self.backThread.start()
 
+    #여기에 Flask에서 메인 서버로 보내는 코드 작성
     def sendToClient(self,result):
         print(json.dumps(result))
         return
-    
 
 worker = AI_Server()
-for img in [os.path.abspath(".")+v for v in ['/V006_77_0_00_01_01_13_0_c03_20201209_0000_S01_1.jpg'
-,'/V006_77_0_00_01_01_13_0_c03_20201209_0188_S01_1.jpg']]:
+img_list = [os.path.abspath(".")+v for v in ['/V006_77_0_00_01_01_13_0_c03_20201209_0000_S01_1.jpg'
+,'/V006_77_0_00_01_01_13_0_c03_20201209_0188_S01_1.jpg']]
+for img in img_list:
+    json_file = json.dumps({
+        "cropImageID": "hello12345",
+        "imageCode": 0
+    })
+    worker.receiveToClient(json_file,img)
+for img in img_list:
+    json_file = json.dumps({
+        "cropImageID": "hello12345",
+        "imageCode": 0
+    })
+    worker.receiveToClient(json_file,img)
+for img in img_list:
     json_file = json.dumps({
         "cropImageID": "hello12345",
         "imageCode": 0
